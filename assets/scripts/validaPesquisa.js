a4291e1e954cbc1baa1d0bacfa92e300ccbf5f15
Marcelo Soares <marcelo.soares92@gmail.com>
2024-09-07 06:21:40 -0300

    Fix: Corrige mensagem de saída da validação

--- assets/scripts/validaPesquisa.js
+++ assets/scripts/validaPesquisa.js
@@ -1,10 +1,10 @@
 function validarPesquisa(campoPesquisa) {
-    const regex = /^[a-zA-Z0-9çáãéêíóôõú]+$/i;
+    const regex = /^[a-zA-Z0-9çáãéêíóôõú]+$/i; //Para permitir espaços acrescente \s no []
     if (!campoPesquisa) {
         alert("Pesquisa inválida! Você precisa digitar o nome de Hashira");
         return false;
     } else if (!regex.test(campoPesquisa)) {
-        alert("Pesquisa inválida! Utilize apenas letras, números e espaços.");
+        alert("Pesquisa inválida! Utilize apenas letras e números.");
         return false;
     }
     return true;
